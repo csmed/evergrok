a326dac5541447459e3fa99c1ad749bb12d1f8ee
csmed <csmedley@gmail.com>
2014-06-30 15:30:16 +1000

    craig

--- js/revise.js
+++ js/revise.js
@@ -1,14 +1,23 @@
 var summary_id    = -1;
-var notes         = {}; // multidimensional array of notes { note_id, bucket, note }
-var visibleIndex  = 0;  // current 'notes' array index for visible note
+var notes         = {}; // multidimensional array of notes { note_id, tray, note, action(0=done, 1=knowIt, 2=forgotIt, 3=reallyKnowIt, -1=skipIt) }
+var trays         = {}; // array of tray qtys
+var qtyDivisor    = 1;  // factor to determine visible tray qtys
+//var visibleIndex  = 0;  // current 'notes' array index for visible note
 var nRevNotes     = 0;  // number of notes to revise
 var nSummaryNotes = 0;  // number of notes in this summary
-var fromBucket    = 0;  // bucket of visible note
 var moreNotes     = true;
+var snackbarTimer;
+
+// constantsW
+var DELETE       = '-2';
+var SKIP         = '-1';
+var DONE         = '0';
+var KNOWIT       = '1';
+var FORGOTIT     = '2';
+var REALLYKNOWIT = '3';
 
 $(document).ready(function() {
-	// fetch a few notes
-	prefetch(true);
+	initSwiper(function() { prefetch(true); });
 });
 
 $( window ).on( "orientationchange", function( event ) {
@@ -24,7 +33,6 @@ $(window).load(function() {
 	setTimeout(function(){
 		scrollTop();
 		setSizes();
-		initSwiper();
 	},100);
 
 	/*
@@ -76,24 +84,28 @@ function log (text) {
 }
 
 function prefetch (initial) {
-	if (moreNotes) {
-		log("prefetch");
+	//log("prefetch(" + initial + ");");
+	var nocache = 0;
+	var activeIndex = mySwiper.activeIndex;
+	if (initial) { nocache = 1; }
+
+	if (moreNotes || initial) {
 		var slideHTML = "";
 		// load at least FIVE notes ahead
-		if (initial || notes.length <= visibleIndex + 5) {
+		if (initial || notes.length <= activeIndex + 5) {
 			$.ajax({
 				type: 'POST',
 				url: 'php/get_summary.php',
-				data: { summary_id: summary_id, fetch_to: (visibleIndex + 5) },
+				data: { summary_id: summary_id,  fetch_to: (activeIndex + 5),  nocache: nocache },
 				dataType: 'json',
 				success: function(j) {
-					log(j);
+					//log(j);
 					// j.title
 					// j.total_count
 					// j.rev_count
 					// j.more
-					// j.notes { note_id, bucket, note }
-					
+					// j.notes { note_id, tray, note }
+
 					// summary info
 					if (initial) {
 						$('#heading').html("<h2>" + j.title + "(" + j.total_count + " notes)</h2>");
@@ -102,28 +114,32 @@ function prefetch (initial) {
 						if (j.more == 0)
 							moreNotes = false;
 
-						// save notes in 'notes' array
+						// save notes & trays in local arrays
 						notes = j.notes;
-						log(notes);
+						trays = j.trays;
+						redrawTrays(true);
 					} else {
 						// append new notes to 'notes' array
 						notes = notes.concat(j.notes);
-						log(notes);
 					}
-					
+
 					// get individual note contents
 					for (i=0; i<j.notes.length; i++) {
 						slideHTML = "<h3>this is a slide</h3><p>with note ID: " + j.notes[i].note_id + "</p><h2>Contents:</h2>" + j.notes[i].note;
 						mySwiper.createSlide(slideHTML).append();
 					}
-					
+
 					// check to see if there is more to come after this set
 					if (j.more == 0) {
 						// append the "slideshow finished" slide
-						slideHTML = $('#shareTemplate').html();
+						slideHTML = $('#shareSlide').html();
 						mySwiper.createSlide(slideHTML).append();
 						moreNotes = false;
 					}
+
+					// scroll to top if this is the first load
+					if (initial)
+						scrollTop();
 				},
 				error: function(x,s,e) {
 					alert(x.responseText);
@@ -133,90 +149,132 @@ function prefetch (initial) {
 	}
 }
 
-// TEST - randomize trays to test visual appearance
-function randomizeTrays () {
+function redrawTrays (initial) {
 	var nCards = 0;
-	var activeTray = notes[visibleIndex].bucket; // Math.floor(Math.random() * 4) + 1; // 1 .. 4
-	var badgeFactor = Math.floor(Math.random() * 5) + 1; // 1 .. 5
-	for (i=0; i<=4; i++) {
+	var maxCards = 0;
+	var activeTray = notes[mySwiper.activeIndex].tray;
+
+	for (i=1; i<=4; i++) {
+		nCards = trays[i];
+		$('#badge' + i).html(nCards);
+		if (nCards > maxCards)
+			maxCards = nCards;
+	}
+
+	if (initial && maxCards > 4)
+		qtyDivisor = Math.ceil(maxCards / 4);
+
+	for (i=1; i<=4; i++) {
+		nCards = Math.round(trays[i] / qtyDivisor);
 		if (i == activeTray) {
-			nCards = Math.floor(Math.random() * 4) + 1; // 1 .. 4
 			$('#tray' + i).attr('src', '/img/cards' + nCards + 'a.png');
 		} else {
-			nCards = Math.floor(Math.random() * 5); // 0 .. 4
 			$('#tray' + i).attr('src', '/img/cards' + nCards + '.png');
 		}
-		$('#badge' + i).html(nCards * badgeFactor);
 	}
 }
 
 function nextNote () {
-	if (visibleIndex < nRevNotes-1) {
-		visibleIndex++;                       // TODO: deal with the possibility of sliding two slides in one go
-		log("visibleIndex=" + visibleIndex);
-		randomizeTrays();
+	var goBack = false;
+	var index = mySwiper.activeIndex -1;
+	scrollTop();
+
+	// act & give feedback
+	log("notes[index].action=" + notes[index].action);
+	switch(notes[index].action) {
+		case DONE:
+			// nothing to do
+			break;
+		case DELETE:
+			// delete 
+			snackbar("slide", "Deleting...", 'UNDO', function() { unDelete(notes[index].note_id); } );
+			notes[index].action = DONE;
+			// TODO: need to remove (hide?) the slide from the swiper
+			//       and have a function to allow for undo (slide re-inserted?)
+			break;
+		case SKIP:
+			// skip and revert note to normal state in case we return and revise later
+			snackbar("slide", "Skipping...", 'MARK AS REVISED', function() { markRevised(index); snackbar("appear", "&#10004; Successfully revised"); } );
+			notes[index].action = KNOWIT;
+			break;
+		case KNOWIT:
+			snackbar("slide", "Successfully revised", 'UNDO', function() { unmarkRevised(index); snackbar("appear", "&#10004; Successfully undone..."); mySwiper.swipePrev(); } );
+			markRevised(index);
+			notes[index].action = DONE;
+			break;
+		case FORGOTIT:
+			snackbar("slide", "OK, we'll revise that again soon", 'UNDO', function() { unmarkForgotIt(index); snackbar("appear", "&#10004; Successfully undone..."); mySwiper.swipePrev(); } );
+			markForgotIt(index);
+			notes[index].action = DONE;
+			break;
+		case REALLYKNOWIT:
+			snackbar("slide", "OK, we won't revise that again for a while", 'UNDO', function() { unmarkReallyKnowIt(index); snackbar("appear", "&#10004; Successfully undone..."); mySwiper.swipePrev(); } );
+			markReallyKnowIt(index);
+			notes[index].action = DONE;
+			break;
+		default:
+			alert("unknown action: " + notes[index].action);
+			log("Error! unknown action=" + notes[index].action + "     note_id=" + notes[index].note_id);
+			break;
+	}
+
+	redrawTrays(false);
+
+	if (index < nRevNotes-1) {
+		//visibleIndex++;                       // TODO: deal with the possibility of sliding two slides in one go
+		//log("visibleIndex=" + index);
 		prefetch(false);
 	} else {
 		log("all done! We need to take you somewhere now");
 		log("user SHOULD be seeing the 'last slide' page now");
+		return false;
 	}
 	return true;
 }
 
 function prevNote () {
-	if (visibleIndex <= 0) {
-		alert("there's no going back");
-		return false;
-	} else {
-		visibleIndex--;                        // TODO: deal with the possibility of sliding two slides in one go
-		randomizeTrays();
-	}
-	log("visibleIndex=" + visibleIndex);
-	//log("noteID=" + noteIDs[visibleIndex]);
+	redrawTrays(false);
 	return true;
 }
-
 
 function skipIt () {
 	//log("skipIt()");
 	hideLessCommon();
+	notes[mySwiper.activeIndex].action = SKIP;
 	mySwiper.swipeNext();
 }
 
-function knowIt (slide) {
-	//log("knowIt(" + slide + ")");
+function knowIt () {
+	//log("knowIt()");
 	hideLessCommon();
-	if (slide)
-		mySwiper.swipeNext();
-	else
-		nextNote();
-	scrollTop();
+	notes[mySwiper.activeIndex].action = KNOWIT;
+	mySwiper.swipeNext();
 }
 
 function reallyKnowIt () {
 	//log("reallyKnowIt()");
 	hideLessCommon();
-    mySwiper.swipeNext()
+	notes[mySwiper.activeIndex].action = REALLYKNOWIT;
+    mySwiper.swipeNext();
 }
 
 function forgotIt () {
 	//log("forgotIt()");
 	hideLessCommon();
+	notes[mySwiper.activeIndex].action = FORGOTIT;
 	mySwiper.swipeNext();
 }
 
 function previous (slide) {
-	log("previous(" + slide + ")");
+	//log("previous(" + slide + ")");
 	hideLessCommon();
-	if (slide)
-		mySwiper.swipePrev()
-	else
-		prevNote();
+	mySwiper.swipePrev();
 }
 
 function deleteIt () {
 	//log("deleteIt()");
 	hideLessCommon();
+	notes[mySwiper.activeIndex].action = DELETE;
 	mySwiper.swipeNext();
 }
 
@@ -231,22 +289,22 @@ function hideMenus () {
 
 function showLessCommon () {
 	$('#clickoutside').show();
-	$('#lesscommon').slideDown();
+	$('#lesscommon').slideDown(300);
 }
 
 function hideLessCommon () {
 	$('#clickoutside').hide();
-	$('#lesscommon').slideUp();
+	$('#lesscommon').slideUp(300);
 }
 
 function showShare () {
 	$('#clickoutside').show();
-	$('#sharemenu').slideDown();
+	$('#sharemenu').slideDown(300);
 }
 
 function hideShare () {
 	$('#clickoutside').hide();
-	$('#sharemenu').slideUp();
+	$('#sharemenu').slideUp(300);
 }
 
 function mainMenu () {
@@ -271,29 +329,6 @@ function lessCommon () {
 	}
 }
 
-function initSwiper () {
-	mySwiper = new Swiper('.swiper-container',{
-		pagination: '.pagination',
-		loop:false,
-		grabCursor: true,
-		paginationClickable: true,
-		onSlideNext: function() { knowIt(false); },
-		onSlidePrev: function() { previous(false); }
-		/*
-		longSwipesRatio: 0.3,
-		moveStartThreshold: 30
-		*/
-	})
-	$('.arrow-left').on('click', function(e){
-		e.preventDefault()
-		mySwiper.swipePrev()
-	})
-	$('.arrow-right').on('click', function(e){
-		e.preventDefault()
-		mySwiper.swipeNext()
-	})
-}
-
 function share (site, url, txt) {
 	var map = { 'email'      : 'mailto:?subject=EverGrok&body=%TXT%%0A%URL%',
 				'facebook'   : 'https://www.facebook.com/sharer/sharer.php?u=%URL%',
@@ -312,6 +347,149 @@ function share (site, url, txt) {
 
 	// TODO: add ga.send() to "/share"
 
+	snackbar("slide", "Opening share link...");
 	window.open(link, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
 }
 
+function snackbar (behaviour, message, action, onclick, actioncolour) {
+	var timeout = 1500;
+
+	// stop any prior animation
+	$('#snackbar').hide();
+	window.clearTimeout(snackbarTimer);
+
+	// setup snackbar
+	$('#snackbarmsg').html(message);
+	if (typeof action === 'undefined' || typeof onclick === 'undefined') {
+		// just a notification
+		$('#snackbaraction').hide();
+	} else {
+		// allow time to click undo
+		timeout = 2000;
+
+		// a notification with an action
+		$('#snackbaraction').show();
+		$('#snackbaraction').html(action);
+		$('#snackbaraction').unbind('click');
+		$('#snackbaraction').bind('click', function() { onclick(); });
+
+		// color of action "button"
+		if (typeof actioncolour !== 'undefined')
+			$('#snackbaraction').css('color', actioncolour);
+	}
+
+	// slide or jump into view
+	switch (behaviour) {
+		case 'appear':
+			$('#snackbar').show();
+			break;
+		case 'slide':
+		default:
+			$('#snackbar').slideDown(300);
+	}
+	// delay, then slide out of view
+	snackbarTimer = window.setTimeout( function() { $('#snackbar').slideUp(300); }, timeout);
+}
+
+function markRevised (index) {
+	var fromTray = parseInt(notes[index].tray);
+	var toTray = fromTray + 1;
+
+	// TODO - AJAX database call
+
+	if (fromTray < 4) {
+		// move note forward one tray
+		notes[index].tray = parseInt(notes[index].tray) +1;
+
+		trays[fromTray] = parseInt(trays[fromTray]) -1;
+		trays[toTray] = parseInt(trays[toTray]) +1;
+	}
+}
+
+function unmarkRevised (index) {
+	var fromTray = parseInt(notes[index].tray);
+	var toTray = fromTray - 1;
+
+	// TODO - AJAX database call
+
+	if (fromTray > 1) {
+		// allow note to be revised again
+		notes[index].action = KNOWIT;
+		// move note backward one tray
+		notes[index].tray = parseInt(notes[index].tray) -1;
+
+		trays[fromTray] = parseInt(trays[fromTray]) -1;
+		trays[toTray] = parseInt(trays[toTray]) +1;
+	}
+}
+
+function markForgotIt (index) {
+	// the card doesn't need to change trays
+
+	// TODO - AJAX database call
+
+	log("markForgotIt(" + index + ");");
+}
+
+function unmarkForgotIt (index) {
+	// the card doesn't need to change trays
+
+	// TODO - AJAX database call
+
+	log("unmarkForgotIt(" + index + ");");
+}
+
+function markReallyKnowIt (index) {
+	var fromTray = parseInt(notes[index].tray);
+	var toTray = fromTray + 2;
+
+	// TODO - AJAX database call
+
+	if (fromTray < 4) {
+		if (fromTray == 3) {
+			// move note to the last tray
+			toTray = 4;
+			notes[index].tray = parseInt(notes[index].tray) +1;
+		} else {
+			// move note forward two trays
+			notes[index].tray = parseInt(notes[index].tray) +2;
+		}
+
+		trays[fromTray] = parseInt(trays[fromTray]) -1;
+		trays[toTray] = parseInt(trays[toTray]) +1;
+	}
+
+	log("markReallyKnowIt(" + index + ");");
+}
+
+function unmarkReallyKnowIt (index) {
+	// need a way to find out the previous tray of the note, then put it back
+
+	// TODO - AJAX database call
+
+	log("unmarkReallyKnowIt(" + index + ");");
+}
+
+function initSwiper (callback) {
+	mySwiper = new Swiper('.swiper-container',{
+		pagination: '.pagination',
+		loop:false,
+		grabCursor: true,
+		paginationClickable: true,
+		onSlideNext: function() { nextNote(); },
+		onSlidePrev: function() { prevNote(); }
+		/*
+		longSwipesRatio: 0.3,
+		moveStartThreshold: 30
+		*/
+	})
+	$('.arrow-left').on('click', function(e){
+		e.preventDefault();
+		mySwiper.swipePrev();
+	})
+	$('.arrow-right').on('click', function(e){
+		e.preventDefault();
+		mySwiper.swipeNext();
+	})
+	callback();
+}
--- php/get_summary.php
+++ php/get_summary.php
@@ -4,28 +4,31 @@
 	// - title       : title of the summary
 	// - total_count : total number of notes in the summary
 	// - rev_count   : total number of notes to revise
-	// - notes       : a selection of the notes to revise
 	// - more        : more notes to come? (boolean)
+	// - notes       : a selection of the notes to revise
+	// - trays       : an array with qty of notes per tray
 	include("util.php");
 	$db      = connectDB();
 	$request = $_POST + $_GET;
 	$return  = array();
 	$user    = getUser();
-	
+
 	// local variables
 	$summaryId = -1;
 	$nReturn   = 0;
 	$chunkSize = 5; // default chunk size
 	$fetchTo   = $chunkSize; // what array index to fetch up to
-	
+	$noCache   = 0;
+
 	// GET/POST variables
 	if (!isset($request['summary_id']) || $request['summary_id'] == "") { nak("summary_id not provided"); }
 	$summaryId = $request['summary_id'];
 	if (isset($request['chunk']) && $request['chunk'] != "")       { $chunkSize = mysql_real_escape_string($request['chunk']); }
 	if (isset($request['fetch_to']) && $request['fetch_to'] != "") { $fetchTo   = mysql_real_escape_string($request['fetch_to']); }
+	if (isset($request['nocache']) && $request['nocache'] != "")   { $noCache   = mysql_real_escape_string($request['nocache']); }
 
-	// return data directly from session if same as prior
-	if (isset($_SESSION['rev_summary_id']) && $summaryId == $_SESSION['rev_summary_id']) {
+	// return data directly from session if same summary as prior
+	if ($noCache == "0" && isset($_SESSION['rev_summary_id']) && $summaryId == $_SESSION['rev_summary_id']) {
 		// return title & total count
 		$return["title"]       = $_SESSION['rev_title'] . ' (from cache)';
 		$return["total_count"] = $_SESSION['rev_total'];
@@ -39,7 +42,7 @@
 			$fetchTo = min($_SESSION['rev_count']-1, max($fetchTo, $_SESSION['rev_index'] + $chunkSize));
 			$return["notes"] = array_slice($_SESSION['rev_notes'], $_SESSION['rev_index']+1, $fetchTo-$_SESSION['rev_index']);
 			$return["more"]  = ( $fetchTo == $_SESSION['rev_count']-1 ? 0 : 1 );
-			
+
 			// save where we've returned to
 			$_SESSION['rev_index'] = $fetchTo;
 		}
@@ -47,7 +50,8 @@
 	// else return data from the database
 	else {
 		// get summary header info
-		$sql = "select $summaryId as summary_id, '12 reasons why Scottie is awesome' as title, 27 as notes";
+ 		$sql = "select $summaryId as summary_id, '12 reasons why Scottie is awesome' as title ";
+		$sql .= " , 27 as notes , 8 as tray1, 5 as tray2, 4 as tray3, 2 as tray4 ";
 		$rows = sql($sql);
 
 		// if summary doesn't exist
@@ -61,28 +65,33 @@
 		// return title
 		$return["title"]       = $rows[0]['title'] . ' (from database)';
 		$return["total_count"] = $rows[0]['notes'];
+		$return["trays"][1]    = $rows[0]['tray1'];
+		$return["trays"][2]    = $rows[0]['tray2'];
+		$return["trays"][3]    = $rows[0]['tray3'];
+		$return["trays"][4]    = $rows[0]['tray4'];
 
 		// get notes
-		$sql = "select 1 as note_id, 'This is note ONE' as note, 2 as bucket ";
-		$sql .= "UNION select 2 as note_id, 'This is note 2' as note, 1 as bucket ";
-		$sql .= "UNION select 3 as note_id, 'This is note 3' as note, 3 as bucket ";
-		$sql .= "UNION select 4 as note_id, 'This is note 4' as note, 1 as bucket ";
-		$sql .= "UNION select 5 as note_id, 'This is note 5' as note, 4 as bucket ";
-		$sql .= "UNION select 6 as note_id, 'This is note 6' as note, 2 as bucket ";
-		$sql .= "UNION select 7 as note_id, 'This is note 7' as note, 1 as bucket ";
-		$sql .= "UNION select 8 as note_id, 'This is note 8' as note, 3 as bucket ";
-		$sql .= "UNION select 9 as note_id, 'This is note 9' as note, 2 as bucket ";
-		$sql .= "UNION select 10 as note_id, 'This is note 10' as note, 1 as bucket ";
-		$sql .= "UNION select 11 as note_id, 'This is note 11' as note, 2 as bucket ";
-		$sql .= "UNION select 12 as note_id, 'This is note 12' as note, 1 as bucket ";
-		$sql .= "UNION select 13 as note_id, 'This is note 13' as note, 3 as bucket ";
-		$sql .= "UNION select 14 as note_id, 'This is note 14' as note, 1 as bucket ";
-		$sql .= "UNION select 15 as note_id, 'This is note 15' as note, 4 as bucket ";
-		$sql .= "UNION select 16 as note_id, 'This is note 16' as note, 2 as bucket ";
-		$sql .= "UNION select 17 as note_id, 'This is note 17' as note, 1 as bucket ";
-		$sql .= "UNION select 18 as note_id, 'This is note 18' as note, 3 as bucket ";
-		$sql .= "UNION select 19 as note_id, 'This is note 19' as note, 2 as bucket ";
-		$sql .= "UNION select 20 as note_id, 'This is note 20' as note, 1 as bucket ";
+		// .action is added to reduce javascript processing time
+		$sql = "select 1 as note_id, 'This is note ONE' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 2 as note_id, 'This is note 2' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 3 as note_id, 'This is note 3' as note, 3 as tray, 1 as action ";
+		$sql .= " UNION select 4 as note_id, 'This is note 4' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 5 as note_id, 'This is note 5' as note, 4 as tray, 1 as action ";
+		$sql .= " UNION select 6 as note_id, 'This is note 6' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 7 as note_id, 'This is note 7' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 8 as note_id, 'This is note 8' as note, 3 as tray, 1 as action ";
+		$sql .= " UNION select 9 as note_id, 'This is note 9' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 10 as note_id, 'This is note 10' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 11 as note_id, 'This is note 11' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 12 as note_id, 'This is note 12' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 13 as note_id, 'This is note 13' as note, 3 as tray, 1 as action ";
+		$sql .= " UNION select 14 as note_id, 'This is note 14' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 15 as note_id, 'This is note 15' as note, 4 as tray, 1 as action ";
+		$sql .= " UNION select 16 as note_id, 'This is note 16' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 17 as note_id, 'This is note 17' as note, 1 as tray, 1 as action ";
+		$sql .= " UNION select 18 as note_id, 'This is note 18' as note, 3 as tray, 1 as action ";
+		$sql .= " UNION select 19 as note_id, 'This is note 19' as note, 2 as tray, 1 as action ";
+		$sql .= " UNION select 20 as note_id, 'This is note 20' as note, 1 as tray, 1 as action ";
 		$rows = sql($sql);
 		$nReturn = min(count($rows), $chunkSize);
 
@@ -104,5 +113,5 @@
 	//print "rev_index: " . $_SESSION['rev_index'] . "<br>";
 	//print "<br>\n";
 
-    ack(json_encode($return), 'get_summary.php request');	
+    ack(json_encode($return), 'get_summary.php request');
 ?>
